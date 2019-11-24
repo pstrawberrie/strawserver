f82ac449709188f5149f058e4404167ef495ab32
Josh <josh@Admins-MacBook-Pro-8.local>
2019-11-24 10:37:30 -0700

    start some stuff

---
+++ client/components/Index/Index.js
@@ -0,0 +1,50 @@
+/**
+ * Index Parent Component
+ */
+
+import React from 'react';
+import io from 'socket.io-client';
+
+import Nav from '../Nav/Nav';
+
+class IndexParent extends React.Component {
+  /**
+   * State
+   */
+  state = {
+    serverStatus: {},
+  }
+
+  /**
+   * Did Mount
+   */
+  componentDidMount() {
+    this.handleSockets();
+  }
+
+  /**
+   * Handle Websockerts
+   */
+  handleSockets() {
+    const socket = io('http://localhost:3000');
+    socket.on('connect', () => {
+      socket.emit('serverStatus', {
+        some: 'data'
+      });
+    });
+    socket.on('serverStatus', (data) => {
+      console.log('Got serverStatus socket from server:', data);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Nav serverStatus={this.state.serverStatus} />
+        <main id="main-content">
+
+        </main>
+      </>
+    );
+  }
+}
--- client/components/Nav/Nav.js
+++ client/components/Nav/Nav.js
@@ -4,69 +4,22 @@
 
 import React from 'react';
 import './Nav.scss';
-import io from 'socket.io-client';
 
 export default class Nav extends React.Component {
-
-  /**
-   * State
-   */
-  state = {
-    user: {},
-    links: [
-      {
-        title: 'Home',
-        url: '/',
-      },
-      {
-        title: 'Error',
-        url: '/errorpageidontexist',
-      },
-    ],
-    serverStatus: {},
-  };
-
-  /**
-   * Did Mount
-   */
-  componentDidMount() {
-    this.handleSockets();
-  }
-
-  /**
-   * Handle Sockets
-   */
-  handleSockets() {
-    const socket = io('http://localhost:3000');
-    socket.on('connect', () => {
-        socket.emit('serverStatus', {
-            some: 'data'
-        });
-    });
-    socket.on('serverStatus', (data) => {
-        console.log('Got serverStatus socket from server:', data);
-    });
-  }
-
-  /**
-   * Render
-   */
   render() {
-    const name = this.state.user.name  || 'User';
-    const links = this.state.links.map((link, index) =>
-      <li key={index}>
-        <a className="nav__main_link" href={link.url}>{link.title}</a>
-      </li>
-    );
-
     return(
       <>
         <ul className="nav__main">
-          {links}
+          <li>
+            <a className="nav__main_link" href="#testlink">Test Link</a>
+          </li>
+          <li>
+            <a className="nav__main_link" href="#testlink2">Test Link 2</a>
+          </li>
         </ul>
         <div className="nav__user">
           <button className="nav__user_trigger">
-            {name}
+            User
           </button>
         </div>
       </>
